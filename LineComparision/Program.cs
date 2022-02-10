f2091e12fea8f34377bab9dde05b434c3a8aca1f
shreevyas <shreevyas0211@gmail.com>
2022-02-10 22:53:18 +0530

    Commit UC1_GeometricLine

---
+++ LineComparision/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using LineComparision;
+
+GeometricLine line1 = new GeometricLine();
+GeometricLine Line2 = new GeometricLine();
+Console.WriteLine("Enter line 1 details: ");
+line1.GetPointsFromUser();
+Console.WriteLine("Enter line 2 details: ");
+Line2.GetPointsFromUser();
+Console.WriteLine("\nLine 1: ");
+line1.Describe();
+Console.WriteLine("Line 2: ");
+Line2.Describe();
